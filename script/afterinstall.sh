529e13b6b1f8347c1091f3f597461ba4cc314517
Niall O Hara <niall@changex.org>
2020-08-17 10:55:10 +0100

    changed app spec user to ec2-user

--- script/afterinstall.sh
+++ script/afterinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Going to try and run airflow docker-compose script"
 
-cd /home/ec2-user/docker-airflow
+cd /home/ec2-user/docker-airflow/
 
 exec "docker-compose -f docker-compose-LocalExecutor.yml up -d"
