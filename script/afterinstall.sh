9ce2e26847fbba527ce4bb500c20dd94add22e28
Niall O Hara <niall@changex.org>
2020-08-17 10:30:31 +0100

    fixed path to docker-compose file

--- script/afterinstall.sh
+++ script/afterinstall.sh
@@ -1,4 +1,4 @@
 echo "Going to try and run airflow docker-compose script"
 
 # execute apache
-exec "docker-compose -f docker-compose-LocalExecutor.yml up -d"
+exec "docker-compose -f ../docker-compose-LocalExecutor.yml up -d"
