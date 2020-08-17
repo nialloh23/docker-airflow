78a794e6080074e6853be0f4264fcf62a9987202
Niall O Hara <niall@changex.org>
2020-08-17 10:47:35 +0100

    fixed bash script

--- script/afterinstall.sh
+++ script/afterinstall.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 echo "Going to try and run airflow docker-compose script"
 
-echo "First cd into project folder"
-cd %HOMEDRIVE%/home/ec2-user/docker-airflow
+cd /home/ec2-user/docker-airflow
 
-# execute apache
 exec "docker-compose -f docker-compose-LocalExecutor.yml up -d"
