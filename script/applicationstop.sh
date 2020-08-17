167dbc99f6b6c9fb4a56bb01f120b6f67ed486c6
Niall O Hara <niall@changex.org>
2020-08-17 13:12:42 +0100

    trying to fix application stop

--- script/applicationstop.sh
+++ script/applicationstop.sh
@@ -2,4 +2,5 @@
 
 echo "Going to stop the airflow web server and postgres database"
 
-docker rm $(docker ps -a -q) || true
+#docker rm -f $(docker ps -a -q) || true
+docker rm -f  docker-airflow_webserver_1 || true && docker rm -f docker-airflow_postgres_1 || true
