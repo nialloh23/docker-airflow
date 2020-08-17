f3fe5276517bdc1d686ac461d8163bc35d7209b2
Niall O Hara <niall@changex.org>
2020-08-17 10:41:09 +0100

    changed afterinstall script run directory

--- script/afterinstall.sh
+++ script/afterinstall.sh
@@ -1,4 +1,7 @@
 echo "Going to try and run airflow docker-compose script"
 
+echo "First cd into project folder"
+cd %HOMEDRIVE%/home/ec2-user/docker-airflow
+
 # execute apache
-exec "docker-compose -f ../docker-compose-LocalExecutor.yml up -d"
+exec "docker-compose -f docker-compose-LocalExecutor.yml up -d"
