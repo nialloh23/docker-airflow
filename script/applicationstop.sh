77f921788d928043900024efee018857b3231def
Niall O Hara <niall@changex.org>
2020-08-17 13:31:18 +0100

    added improved container stop command

--- script/applicationstop.sh
+++ script/applicationstop.sh
@@ -2,13 +2,5 @@
 
 echo "Going to stop the airflow web server and postgres database"
 
-#docker rm -f $(docker ps -a -q) || true
-#docker rm -f  docker-airflow_webserver_1 || true && docker rm -f docker-airflow_postgres_1 || true
 
-if [ ! "$(docker ps -q -f name=docker-airflow_webserver_1)" ]; then
-        docker rm docker-airflow_webserver_1
-    fi
-
-if [ ! "$(docker ps -q -f name=docker-airflow_postgres_1)" ]; then
-        docker rm docker-airflow_webserver_1
-    fi
+docker rm -f  docker-airflow_webserver_1 || true && docker rm -f docker-airflow_postgres_1 || true
